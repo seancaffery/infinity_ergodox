7c460d15ec0d9ac18dbf9528ef363537b3f03cd1
Fred Sundvik <fsundvik@gmail.com>
2016-02-28 15:55:38 +0200

    Keymap layout changed for easier debugging

--- keymap_plain.c
+++ keymap_plain.c
@@ -16,15 +16,21 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *                                      `-----------'
      */
     [0] = KEYMAP(
-    ESC, 1,   2,   3,   4,   5,   NO,   \
-    TAB, Q,   W,   E,   R,   T,   NO,   \
-    LCTL,A,   S,   D,   F,   G,         \
-    LSFT,Z,   X,   C,   V,   B,   NO,       NO,  NO, \
-    NO,  NO,  NO,  NO,  NO,            BSPC,SPC, NO, \
-                                                 NO),
+    ESC ,   1,   2,   3,   4,   5,  NO, \
+    TAB ,   Q,   W,   E,   R,   T, FN1, \
+    LCTL,   A,   S,   D,   F,   G,      \
+    LSFT,   Z,   X,   C,   V,   B,BTLD,        NO,  NO, \
+    RSFT,  NO,  NO,  NO,  NO,           BSPC, SPC,  NO, \
+                                                    NO),
+    [1] = KEYMAP(
+    TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+    TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+    TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,      \
+    TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,      TRNS,TRNS,  \
+    TRNS,TRNS,TRNS,TRNS,TRNS,           TRNS,TRNS,TRNS,  \
+                                                  TRNS),
 };
 
 const uint16_t fn_actions[] = {
     [0] = ACTION_LAYER_MOMENTARY(1),
 };
-
