9129ce58106e320365df7d2931d40771cdad6b06
Fred Sundvik <fsundvik@gmail.com>
2016-02-28 21:47:20 +0200

    Improve serial link initialization

--- serial_link_hal.c
+++ serial_link_hal.c
@@ -14,18 +14,14 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-
-#include "keyboard.h"
-#include "action_layer.h"
-#include "visualizer.h"
-#include "host.h"
+#include "ch.h"
+#include "hal.h"
 #include "serial_link/system/driver.h"
 
-void post_keyboard_init(void) {
-    visualizer_init();
+void init_serial_link_hal(void) {
+    PORTA->PCR[1] = PORTx_PCRn_PE | PORTx_PCRn_PS | PORTx_PCRn_PFE | PORTx_PCRn_MUX(2);
+    PORTA->PCR[2] = PORTx_PCRn_DSE | PORTx_PCRn_SRE | PORTx_PCRn_MUX(2);
+    PORTE->PCR[0] = PORTx_PCRn_PE | PORTx_PCRn_PS | PORTx_PCRn_PFE | PORTx_PCRn_MUX(3);
+    PORTE->PCR[1] = PORTx_PCRn_DSE | PORTx_PCRn_SRE | PORTx_PCRn_MUX(3);
 }
 
-void post_keyboard_task() {
-    serial_link_update();
-    visualizer_set_state(default_layer_state, layer_state, host_keyboard_leds());
-}
