60c38603fe3abf1a21d78cac32c5650316856363
Fred Sundvik <fsundvik@gmail.com>
2016-03-13 22:43:04 +0200

    Fixup license and copyright headers

--- lcd_backlight_hal.c
+++ lcd_backlight_hal.c
@@ -1,5 +1,6 @@
 /*
 Copyright 2016 Fred Sundvik <fsundvik@gmail.com>
+Jun Wako <wakojun@gmail.com>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
